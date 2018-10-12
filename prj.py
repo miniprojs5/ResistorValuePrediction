efe30f922a6047d27fa3f7eab28d01c9905430f7
Gayathri <gaya3vs.0007@gmail.com>
2018-10-12 10:11:13 +0000

    access and resize as a single code

--- prj.py
+++ prj.py
@@ -16,3 +16,21 @@ print('Resized Dimensions : ',resized.shape)
 cv2.imshow("Resized image", resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+#========================================================
+#accessed all files in a folder AND RESIZED TO 200 100
+
+import cv2
+import os
+import glob
+img_dir = "/home/user/resistor/test/*.jpg"  
+data_path = os.path.abspath(img_dir)
+files = glob.glob(data_path)
+print(files)
+width=250
+height=100
+for f in files:
+    img=cv2.imread(f,cv2.IMREAD_UNCHANGED)
+    dim=(width,height)
+    resized=cv2.resize(img,dim,interpolation=cv2.INTER_AREA)
+    print("resized dimensions",resized.shape)
+    
