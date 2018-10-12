75dd30ffd34da8013ef074cc5d37c177f58f2748
Gayathri <gaya3vs.0007@gmail.com>
2018-10-12 11:13:39 +0000

    Resized to 200 100 successfully

--- prj.py
+++ prj.py
@@ -1,21 +1,24 @@
-import cv2
+
+#import cv2
  
-img = cv2.imread('/home/user/resistor/res.jpeg', cv2.IMREAD_UNCHANGED)
+#img = cv2.imread('/home/user/resistor/res.jpeg', cv2.IMREAD_UNCHANGED)
  
-print('Original Dimensions : ',img.shape)
+#print('Original Dimensions : ',img.shape)
  
-width = 200
-height = 100
-dim = (width, height)
+#width = 200
+#height = 100
+#dim = (width, height)
  
-# resize image
-resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+# ===============resize image
+#resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
  
-print('Resized Dimensions : ',resized.shape)
+#print('Resized Dimensions : ',resized.shape)
  
-cv2.imshow("Resized image", resized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.imshow("Resized image", resized)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+
 #========================================================
 #accessed all files in a folder AND RESIZED TO 200 100
 
